297ed25515c150aaf8a392c08f62f3e43fb472a8
mikhail pogodin <bl365@bk.ru>
2020-11-24 01:08:38 +1000

    module 3, task 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -4,4 +4,30 @@
  */
 function camelize(str) {
   // ваш код...
+  let shortStr = '',
+	stSlice = 0,
+	endSlice = 0,
+	indOf = 0;
+
+	while  (indOf !== -1) {
+		indOf = str.indexOf('-', stSlice)
+
+		if (indOf === -1) {
+			endSlice = str.length
+		} else {
+			endSlice = indOf
+		}
+
+		if (stSlice===0) {
+			shortStr += str.slice(stSlice, endSlice)
+		} else {
+
+			shortStr += str[stSlice].toUpperCase()
+		+ str.slice(stSlice+1, endSlice)
+		}
+
+		stSlice = endSlice +1
+	}
+
+	return shortStr;
 }
