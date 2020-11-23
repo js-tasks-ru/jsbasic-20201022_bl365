97c5810a380e2d4475b1fb28b513c80883b3682f
mikhail pogodin <bl365@bk.ru>
2020-11-24 01:04:53 +1000

    module 3, task 1,2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -6,4 +6,12 @@
  */
 function filterRange(arr, a, b) {
   // ваш код...
+  let temp, res = [];
+	for (let i = 0; i < arr.length; i++) {
+		temp = arr[i];
+		if (temp >= a && temp <= b ) {
+			res.push(temp);
+		}
+	}
+	return res;
 }
