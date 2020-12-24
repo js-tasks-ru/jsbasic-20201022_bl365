1423924165245f8c8456da8d996c617edc211076
mikhail pogodin <bl365@bk.ru>
2020-12-25 02:23:19 +1000

    refactoring: clear print

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -12,7 +12,6 @@ let calculator = {
     return calculator.a * calculator.b;
   }
 };
-print(calculator)
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
