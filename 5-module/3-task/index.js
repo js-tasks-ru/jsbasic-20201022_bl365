6d6b3e276cfe89e3007de838f4882129aeedd16d
mikhail pogodin <bl365@bk.ru>
2021-01-27 18:23:10 +1000

    5module 3task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+  let btnGoRight = document.querySelector('.carousel__arrow_right')
+  let btnGoLeft = document.querySelector('.carousel__arrow_left')
+  let elementIndent = document.querySelector('.carousel__inner').getBoundingClientRect().x
+  let elemWidth = document.querySelector('.carousel__inner').offsetWidth
+  let positionElem = document.querySelector('.carousel__inner').getBoundingClientRect().x
+  let slideNumber = 0, res
+
+  btnGoLeft.style.display = 'none'
+
+  function clickRight() {
+    slideNumber++
+
+    if (slideNumber === 1) {
+      btnGoLeft.style.display = ''
+    } else if (slideNumber === 3) {
+      btnGoRight.style.display = 'none'
+    }
+
+    return positionElem - (elemWidth* slideNumber) -elementIndent
+  }
+
+  function clickLeft() {
+    slideNumber--
+
+    if (slideNumber === 2) {
+      btnGoRight.style.display = ''
+    } else if (slideNumber === 0) {
+      btnGoLeft.style.display = 'none'
+    }
+
+    return -(positionElem + (elemWidth* slideNumber) -elementIndent)
+  }
+
+  btnGoRight.addEventListener('click', () => {document.querySelector('.carousel__inner').style.transform = `translateX(${clickRight()}px)`})
+  btnGoLeft.addEventListener('click', () =>  {document.querySelector('.carousel__inner').style.transform = `translateX(${clickLeft()}px)`})
 }
