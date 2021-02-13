388b6532e369463215063ba9d1869eb5c93ebbdf
mikhail pogodin <bl365@bk.ru>
2021-02-13 15:12:08 +1000

    the table is created and filled

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -5,30 +5,46 @@
  * Пример одного элемента, описывающего строку таблицы
  *
  *      {
-     *          name: '',
-     *          age: 25,
-     *          salary: '1000',
-     *          city: 'Petrozavodsk'
-     *   },
- *
- * @constructor
- */
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
  *          name: 'Ilia',
  *          age: 25,
  *          salary: '1000',
  *          city: 'Petrozavodsk'
- *      },ы
+ *      },{
+ *          name: 'Petr',
+ *          age: 27,
+ *          salary: '1200',
+ *          city: 'Petropavlovsk'
+ *      },
  *
  * @constructor
  */
 export default class UserTable {
   constructor(rows) {
+    let listTHTickets = {name: 'Имя', age: 'Возраст', salary: 'Зарплата', city: 'Город', empty: ''}
+    let tbl = document.createElement('table')
+
+
+    for (let i in rows) {
+      tbl.insertRow()
+      for (let j in rows[i]) {
+        tbl.rows[i].insertCell().textContent = rows[i][j]
+      }
+      tbl.rows[i].insertCell().innerHTML = '<button>X</button>'
+    }
+
+    tbl.createTHead().insertRow(0)
+    for (let ticket in listTHTickets) {
+      let th = document.createElement('th')
+      th.innerText = listTHTickets[ticket]
+      tbl.tHead.rows[0].appendChild(th)
+    }
+
+    this.elem = tbl
   }
+
+
+
+
+
+
 }
