0ad8e91bc63ac4e476b98f3e7dea446c8ef88a0d
mikhail pogodin <bl365@bk.ru>
2021-03-02 22:28:11 +1000

    6mod 3task complete

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,10 +3,72 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.currentSlideNumber = 0 // номер текущего слайда
     this.render();
   }
 
-  slidesGenetation() { //генератор слайдов
+  render() {
+    let elem = document.createElement('div') // создание корневого элемента .carousel
+    elem.className = 'carousel'
+
+    // генерация стрелок. Левая создаётся скрытой
+    elem.innerHTML += `
+      <div class="carousel__arrow carousel__arrow_right">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </div>
+      <div class="carousel__arrow carousel__arrow_left" style="display:none">
+        <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+      </div>
+    `
+    elem.addEventListener('click', ({target}) => this.onClick(target))
+
+    this.elem = elem
+    this.slidesGeneration()
+  }
+
+  onClick(target) {
+    if(target.closest('.carousel__arrow')) {
+      this.switchSlides(target)
+    }
+
+    if(target.closest('.carousel__button')) {
+      this.productAdd(target)
+    }
+  }
+
+  switchSlides(target) {
+    let slidesAmount = this.slides.length
+    let carouselInnerElem = this.elem.querySelector('.carousel__inner')
+    let leftArrow = this.elem.querySelector('.carousel__arrow_left')
+    let rightArrow = this.elem.querySelector('.carousel__arrow_right')
+
+    if(target.closest('.carousel__arrow_left')) {
+      update(--this.currentSlideNumber)
+    }
+
+    if(target.closest('.carousel__arrow_right')) {
+      update(++this.currentSlideNumber)
+    }
+
+    function update(currentSlideNumber) {
+      let offset = -carouselInnerElem.offsetWidth * currentSlideNumber
+      carouselInnerElem.style.transform = `translateX(${offset}px)`
+
+      if(currentSlideNumber == slidesAmount - 1) {
+        rightArrow.style.display = 'none'
+      } else (
+        rightArrow.style.display = ''
+      )
+
+      if(currentSlideNumber == 0) {
+        leftArrow.style.display = 'none'
+      } else {
+        leftArrow.style.display = ''
+      }
+    }
+  }
+
+  slidesGeneration() {
     let carouselInner = document.createElement('div') // создаётся div для списка слайдов
     carouselInner.className = 'carousel__inner'
 
@@ -25,25 +87,13 @@ export default class Carousel {
                                   </div>`
       carouselInner.appendChild(carouselSlide) // добавление созданного слайда в список слайдов
     }
-    this.carousel.appendChild(carouselInner)  // добавление списка слайдов в корневой элемент carousel
+    this.elem.appendChild(carouselInner)  // добавление списка слайдов в корневой элемент .carousel
   }
 
-  render() {
-    let carousel = document.createElement('div') // создание корневого элемента carousel
-
-    this.carousel = carousel
-    carousel.className = 'carousel'
-
-    // добавление кнопок. Вероятно, придётся переделать, для привязки событий на кнопки
-    carousel.innerHTML += `
-      <div class="carousel__arrow carousel__arrow_right">
-        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
-      </div>
-      <div class="carousel__arrow carousel__arrow_left">
-        <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
-      </div>
-    `
-    this.slidesGenetation()
-    this.elem = carousel
+  productAdd(target) {
+    this.elem.dispatchEvent(new CustomEvent("product-add", {
+      detail: target.closest('.carousel__slide').getAttribute('data-id'),
+      bubbles: true
+    }))
   }
 }
